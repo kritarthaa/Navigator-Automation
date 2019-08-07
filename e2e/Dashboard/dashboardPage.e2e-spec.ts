84a1b0155a65b724b908cc1032bd5b42777d50cf
kritarthaa <akritartha@gmail.com>
2019-08-07 13:32:16 +0545

    Dashboard done

--- e2e/Dashboard/dashboardPage.e2e-spec.ts
+++ e2e/Dashboard/dashboardPage.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from '../app.po';
 import { browser, ExpectedConditions } from 'protractor';
+import { async } from 'q';
 
 describe('Dashboard Page', () => {
   let app = new AppPage();
@@ -16,13 +17,15 @@ describe('Dashboard Page', () => {
     await app.notification.closeBtn().click();
 
   });
-
+// only navigates
   it('Only navigates', async () => {
     await browser.waitForAngularEnabled(false);
     await(browser.wait(ExpectedConditions.visibilityOf(app.dashboard.dashboardBtn())));
     await app.dashboard.dashboardBtn().click();
     await(browser.wait(ExpectedConditions.visibilityOf(app.dashboard.dashboardHeader), 10000));
   });
+
+  // Verify contents of the dashboard page
 
   it('Verify contents of dashboard page', async () => {
 
@@ -38,6 +41,7 @@ describe('Dashboard Page', () => {
 
 
   });
+  // Verify contents from 'today's Stats wizard
 
   it('Verify contents of todays stats', async () => {
 
@@ -49,6 +53,40 @@ describe('Dashboard Page', () => {
     await app.dashboard.bestSellerTab.isDisplayed();
 
   });
+
+  // Verify contents from Today's Stats tabs
+  // Verify contents from Today's Stats > Sales & Earnings Today
+  // Verify contents from Today's Stats > Best Seller
+
+
+  it('Verify contents of tabs', async () => {
+
+    await browser.waitForAngularEnabled(false);
+    await(browser.wait(ExpectedConditions.visibilityOf(app.dashboard.dashboardBtn())));
+    await app.dashboard.dashboardBtn().click();
+    await(browser.wait(ExpectedConditions.visibilityOf(app.dashboard.salesEarningTab), 10000));
+    await(browser.wait(ExpectedConditions.visibilityOf(app.dashboard.salesToday), 10000));
+    await app.dashboard.bestSellerTab.click();
+    await(browser.wait(ExpectedConditions.visibilityOf(app.dashboard.bestSeller), 10000));
+
+  });
+
+  // Verify contents from '10 Most Recent Orders' table
+  // Click '10 Most Recent Orders' table -> 'View All' link
+
+  it('Verify Most recent orders', async () => {
+    await browser.waitForAngularEnabled(false);
+    await app.dashboard.dashboardBtn().click();
+    await(browser.wait(ExpectedConditions.visibilityOf(app.dashboard.mostRecentOrdersHeader), 10000));
+    await(browser.wait(ExpectedConditions.visibilityOf(app.dashboard.viewOrderBtn), 10000));
+    await app.dashboard.viewOrderBtn.click();
+    await(browser.wait(ExpectedConditions.urlContains('Order/Details')));
+
+
+  });
+
+
+
 
 
 });
